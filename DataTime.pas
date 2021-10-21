916764a3e79b2e5e909bf485c03b6f6e6bcc18b9
Elizaveta Dombayan <edombaian@sfedu.ru>
2021-10-21 09:32:44 +0300

    fix DataTime.pas

--- DataTime.pas
+++ DataTime.pas
@@ -1,3 +1,12 @@
-﻿begin
+﻿function LaterInYear(day1, month1, day2, month2 : integer): (integer,integer);
+begin
+  if month1 = month2 then
+    if day1 > day2 then
+      result := (day1, month1)
+    else result := (day2, month2);
+  if month1 > month2 then result := (day1, month1);
+  if month1 < month2 then result := (day2, month2);
+end;
+begin
   
 end.
